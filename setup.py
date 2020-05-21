51cf2b5071c722c7b3b1dfe8739ebefd8ed14323
Sean Floyd <seanfloyd@icloud.com>
2020-05-21 06:16:55 -0400

    Setup.py build process
    - added a setup command to convert the API endpoints JSON file to an importable python file.
    - added the build directory to `.gitignore`

--- setup.py
+++ setup.py
@@ -1,16 +1,46 @@
+import os
 from setuptools import setup
+from distutils.command.build_py import build_py
+
+
+def generate_api_endpoints_py_content(directory):
+      import json
+      filename = 'api_endpoints'
+      source_file = filename + '.json'
+      new_file = os.path.join(directory, filename + '.py')
+      with open(source_file, 'r') as endpoints_file:
+            source_file_contents = endpoints_file.read()
+            parsed_source_file_contents = json.loads(source_file_contents)
+            new_file_content = filename.upper() + '=' + str(parsed_source_file_contents)
+            with open(new_file, 'w') as api_endpoints_py:
+                  api_endpoints_py.write(new_file_content)
+
+
+# https://digip.org/blog/2011/01/generating-data-files-in-setup.py.html
+class my_build_py(build_py):
+      def run(self):
+        # honor the --dry-run flag
+        if not self.dry_run:
+            self.mkpath(self.build_lib)
+            generate_api_endpoints_py_content(self.build_lib)
+
+        # distutils uses old-style classes, so no super()
+        build_py.run(self)
+
 
 setup(name='tinder_api',
-      version='2020.5',
-      description='Tinder API for Python',
+      version='2020.5.21.1420',
+      description='Tinder API wrapper in Python',
       long_description=open('README.md').read().strip(),
-      author='fabien bessez',
-      url='https://github.com/fbessez/Tinder',
-      py_modules=['tinder_api', 'facebook_auth_token'],
+      author='Sean Floyd',
+      maintainer='Sean Floyd',
+      url='https://github.com/SeanLF/Tinder',
+      py_modules=['tinder_api', 'facebook_auth_token', 'helpers'],
       install_requires=['requests',
                         'robobrowser',
                         'lxml'],
       license='MIT License',
       zip_safe=False,
-      keywords=['tinder-api', 'tinder', 'python-3', 'robobrowser'],
+      keywords=['tinder-api', 'tinder', 'python-3'],
+      cmdclass={'build_py': my_build_py},
      )
